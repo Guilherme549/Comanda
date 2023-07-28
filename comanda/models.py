9a4abe977c15709ce37172b6800c692feca493f4
Guilherme Francisco Alves Ribeiro <101433358+Guilherme549@users.noreply.github.com>
2023-07-28 14:50:11 -0300

    Criando models

--- comanda/models.py
+++ comanda/models.py
@@ -2,4 +2,29 @@ from django.db import models
 
 
 class Comanda(models.Model):
-    mesa = models.ForeignKey(to, )
+    mesa = models.IntegerField()
+    cerveja = models.CharField(max_length=20, null=True)
+    refrigerante = models.CharField(max_length=20, null=True)
+    espetinho = models.CharField(max_length=20, null=True)
+
+    # Qtd
+
+    cervejaQtd = models.IntegerField(null=True)
+    refrigeranteQtd = models.IntegerField(null=True)
+    espetinhoQtd = models.IntegerField(null=True)
+
+    # preco
+
+    cervejaPrecoTotal = models.DecimalField(max_digits=10, decimal_places=2, null=True)
+    refrigerantePrecoTotal = models.DecimalField(
+        max_digits=10, decimal_places=2, null=True
+    )
+    espetinhoPrecoTotal = models.DecimalField(
+        max_digits=10, decimal_places=2, null=True
+    )
+
+    precoTotal = models.DecimalField(max_digits=10, decimal_places=2, null=True)
+
+
+    def __str__(self):
+        return self.mesa
--- comanda/views.py
+++ comanda/views.py
@@ -1,11 +1,37 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Comanda
 
 
 def visualizarPedidos(request):
     return render(request, "index.html")
 
 
-
 def comanda(request):
-    return render(request, "pedido.html")
+    if request.method == "GET":
+        return render(request, "pedido.html")
+    elif request.method == "POST":
+        mesa = request.POST.get("mesa")
+        cerveja = request.POST.get("cerveja")
+        qtdCerveja = int(request.POST.get("qtdCerveja"))
+        refrigerante = request.POST.get("refrigerantes")
+        qtdRefrigerante = int(request.POST.get("qtdRefrigerante"))
+        espetinho = request.POST.get("espetinhos")
+        qtdEspetinho = int(request.POST.get("qtdEspetinho"))
+        precoTotal = request.POST.get("precoTotal")
+
+        comanda = Comanda(
+            mesa=mesa,
+            cerveja=cerveja,
+            cervejaQtd=qtdCerveja,
+            refrigerante=refrigerante,
+            refrigeranteQtd=qtdRefrigerante,
+            espetinho=espetinho,
+            espetinhoQtd=qtdEspetinho,
+            precoTotal=precoTotal,
+        )
+
+        comanda.save()
+        pedido = Comanda.objects.all()
+        return render(request, "index.html", {'pedido': pedido})
 
