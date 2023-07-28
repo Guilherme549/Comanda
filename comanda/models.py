cc9df2dea0f97fb1b7ccf34f8db26b1b72b3f220
Guilherme Francisco Alves Ribeiro <101433358+Guilherme549@users.noreply.github.com>
2023-07-28 09:59:24 -0300

    .

--- comanda/models.py
+++ comanda/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+
+class Comanda(models.Model):
+    mesa = models.ForeignKey(to, )
--- comanda/views.py
+++ comanda/views.py
@@ -5,5 +5,7 @@ def visualizarPedidos(request):
     return render(request, "index.html")
 
 
+
 def comanda(request):
     return render(request, "pedido.html")
+
