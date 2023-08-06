ca56c7c7c2702e7972905ddf32581174a97c7bac
Guilherme Francisco Alves Ribeiro <101433358+Guilherme549@users.noreply.github.com>
2023-08-06 16:26:15 -0300

    Adicionando funcionalidade para barra de pesquisa

--- comanda/urls.py
+++ comanda/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path("", views.visualizarPedidos, name="visualizarPedidos"),
     path("comanda/", views.comanda, name="comanda"),
     path("pedido/<int:id>", views.pedido, name="pedido"),
+    path("atualizar-pedido/<int:id>", views.atualizarPedido, name="atualizarPedido"),
+    path("pesquisarMesa", views.pesquisarMesa, name="pesquisarMesa"),
 ]
--- comanda/views.py
+++ comanda/views.py
@@ -5,7 +5,7 @@ from .models import Comanda
 import json
 from django.contrib import messages
 from django.contrib.messages import constants
-
+from decimal import Decimal
 
 def visualizarPedidos(request):
     pedidos = Comanda.objects.all()
@@ -64,3 +64,50 @@ def pedido(request, id):
     elif request.method == "POST":
         comanda.delete()
         return redirect(reverse("visualizarPedidos"))
+
+
+def atualizarPedido(request, id):
+    comanda = get_object_or_404(Comanda, id=id)
+    if request.method == "GET":
+        return render(request, "atualizarPedido.html", {"comanda": comanda})
+    elif request.method == "POST":
+        cerveja = request.POST.get("cerveja")
+        qtdCerveja = int(request.POST.get("qtdCerveja"))
+        refrigerante = request.POST.get("refrigerantes")
+        qtdRefrigerante = int(request.POST.get("qtdRefrigerante"))
+        espetinho = request.POST.get("espetinhos")
+        qtdEspetinho = int(request.POST.get("qtdEspetinho"))
+        precoTotal = request.POST.get("precoTotal")
+
+        listaSaboresBackend = request.POST.get("listaSaboresBackend")
+        lista_sabores_backend = json.loads(listaSaboresBackend)
+
+        # Atualiza os dados do objeto com os novos valores
+        comanda.cerveja = cerveja
+        comanda.cervejaQtd = qtdCerveja
+        comanda.refrigerante = refrigerante
+        comanda.refrigeranteQtd = qtdRefrigerante
+        comanda.espetinho = espetinho
+        comanda.espetinhoQtd = qtdEspetinho
+        precoTotal_decimal = Decimal(precoTotal)
+        comanda.precoTotal += precoTotal_decimal
+        comanda.listaItensSelecionados += lista_sabores_backend
+
+        # Salva as alterações no banco de dados
+        comanda.save()
+
+        return render(request, "pedidoCliente.html", {"comanda": comanda})
+
+        
+def pesquisarMesa(request):
+    if request.method == "GET":
+        mesa = request.GET.get("mesa")
+        if mesa:
+            pedidos = Comanda.objects.filter(mesa__icontains=mesa)
+        else:
+            pedidos = Comanda.objects.all()
+
+        return render(request, "index.html", {"pedidos": pedidos})
+    else:
+        return redirect(reverse("visualizarPedidos"))
+
